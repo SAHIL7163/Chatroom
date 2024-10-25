aeaf8f0e331a79da88c1457de8f8f94f7c7ef536
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-10-26 01:14:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 3500
+
 const ADMIN = "Admin"
 
 const app = express();
